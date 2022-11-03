ba2c3071e9121f988952f9c57d87d0efc7c65abd
IsmaeBaena <ismael.baena.contacto@gmail.com>
2022-11-03 12:06:38 +0100

    Ejercicio JavaScript 9

---
+++ 6.-JavaScript/[Ej-9]_Math_Methods/main.js
@@ -0,0 +1,5 @@
+console.log(Math.floor(Math.random()*100));
+
+const NUMDECIMALS = 3;
+var decimalPlacesFactor =Math.pow(10, NUMDECIMALS);
+console.log(Math.floor(Math.PI*decimalPlacesFactor)/decimalPlacesFactor);
